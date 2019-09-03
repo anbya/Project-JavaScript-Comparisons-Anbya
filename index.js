966d705051f01de8c85b80dd48183dff0dc4ff6c
anbya <anbyaalibia@gmail.com>
2019-09-03 12:18:41 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+console.log('Hello World')
+console.warn('Hello Warning')
+console.error('Hello Error')
+console.info('Hello Info')
+
+alert('Hello Alert')
+
+console.log('Hello World')
+
+const name = 'ANBYA ARMY ALI'
+
+console.log(`Hello ${name}`)
+
+alert(`Hello ${name}`)
+
+console.log(0)
+console.log(100)
+console.log(3.14)
+
+const age = 28
+
+console.log(`His age is ${age}`)
+
+alert(`His age is ${age}`)
+
+console.log({ name: 'ANBYA', age: 28, superPower: true })
+console.log({
+  name: 'ANBYA',
+  age: 28,
+  superPower: true
+})
+
+const hero = { name: 'ANBYA', age: 28, superPower: true }
+
+console.log(hero)
+console.log(hero['name'])
+console.log(hero.age)
+console.log(hero.superPower)
+
+alert(hero)
+alert(hero['name'])
+alert(hero.age)
+alert(hero.superPower)
+
+console.log([1, 2, 3])
+console.log([0, 1, 2, 3])
+console.log(['Alpha', 100, true])
+
+const person = ['Alpha', 100, true]
+
+console.log(person)
+console.log(person[0])
+console.log(person[1])
+console.log(person[2])
+
+alert(person)
+alert(person[0])
+alert(person[1])
+alert(person[2])
+
+console.log(true)
+console.log(false)
+console.log(true, false)
+
+const toggle = false
+
+console.log(`The lamp's toggle is ${toggle}`)
+
+alert(`The lamp's toggle is ${toggle}`)
